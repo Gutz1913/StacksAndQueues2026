caa12fe05b37f9e71125399587f5dd21031fc255
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-17 12:01:16 -0500

    UI Console for Stacks using a list

---
+++ StacksAndQueues/StackUsingList.UI.Console/Program.cs
@@ -0,0 +1,45 @@
+﻿using Stacks.Backend;
+
+var stack = new StackUsingList<string>();
+var opt = string.Empty;
+
+try
+{
+    do
+    {
+        opt = Menu();
+        switch (opt)
+        {
+            case "1":
+                Console.Write("Enter item: ");
+                stack.Push(Console.ReadLine()!);
+                break;
+
+            case "2":
+                Console.WriteLine(stack.Peek());
+                break;
+
+            case "3":
+                Console.WriteLine(stack.Pop());
+                break;
+
+            default:
+                break;
+        }
+    }
+    while (opt != "0");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+string Menu()
+{
+    Console.WriteLine("1. Push");
+    Console.WriteLine("2. Peek");
+    Console.WriteLine("3. Pop");
+    Console.WriteLine("0. Exit");
+    Console.Write("Select your option: ");
+    return Console.ReadLine()!;
+}
