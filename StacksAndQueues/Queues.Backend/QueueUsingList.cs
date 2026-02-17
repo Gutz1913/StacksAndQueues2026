93dce450c4ee2b141fc60a57cd90b1b2ffc0d5ac
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-17 11:04:34 -0500

    Queue using a list

--- StacksAndQueues/Queues.Backend/QueueUsingList.cs
+++ StacksAndQueues/Queues.Backend/QueueUsingList.cs
@@ -1,5 +1,36 @@
 ﻿namespace Queues.Backend;
 
-public class QueueUsingList
+public class QueueUsingList<T>
 {
+    private List<T> _elements;
+
+    public QueueUsingList()
+    {
+        _elements = [];     //New List<T>();
+    }
+
+    public void Enqueue(T item)
+    {
+        _elements.Add(item);        //Add item to the end of the list
+    }
+
+    public T Dequeue()
+    {
+        if (_elements.Count == 0)
+        {
+            throw new Exception("Queue is empty");
+        }
+        var item = _elements[0];    //Get the first item
+        _elements.RemoveAt(0);      //Remove the first item
+        return item;                //Return the dequeued item
+    }
+
+    public T Peek()
+    {
+        if (_elements.Count == 0)
+        {
+            throw new Exception("Queue is empty");
+        }
+        return _elements[0];
+    }
 }
