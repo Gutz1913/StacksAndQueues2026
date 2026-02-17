b3d54df5d1599a4b907114b9dc9810827c14f133
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-17 11:52:31 -0500

    Class Stack.Backend and stack using an array

---
+++ StacksAndQueues/Stacks.Backend/StackUsingArray.cs
@@ -0,0 +1,40 @@
+﻿namespace Stacks.Backend;
+
+public class StackUsingArray<T>
+{
+    private T[] _elements = null!;
+    private int _top;
+
+    public StackUsingArray(int capacity)
+    {
+        _elements = new T[capacity];
+        _top = -1;
+    }
+
+    public void Push(T item)
+    {
+        if (_top == _elements.Length - 1)
+        {
+            throw new Exception("Stack is full.");
+        }
+        _elements[++_top] = item;
+    }
+
+    public T Pop()
+    {
+        if (_top == -1)
+        {
+            throw new Exception("Stack is empty");
+        }
+        return _elements[_top--];
+    }
+
+    public T Peek()
+    {
+        if (_top == -1)
+        {
+            throw new Exception("Stack is empty");
+        }
+        return _elements[_top];
+    }
+}
