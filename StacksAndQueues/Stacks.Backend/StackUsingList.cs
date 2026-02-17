c6aba0d385c5c3df2db70a8f536ee6e7dc3ded31
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-17 11:54:30 -0500

    Class Stack.Backend and Stack using a list

---
+++ StacksAndQueues/Stacks.Backend/StackUsingList.cs
@@ -0,0 +1,28 @@
+﻿namespace Stacks.Backend;
+
+public class StackUsingList<T>
+{
+    private List<T> _elements = [];   //new List<T>() = []
+
+    public void Push(T item) => _elements.Add(item);
+
+    public T Pop()
+    {
+        if (_elements.Count == 0)
+        {
+            throw new Exception("Stack is empty.");
+        }
+        T item = _elements[_elements.Count - 1];
+        _elements.RemoveAt(_elements.Count - 1);
+        return item;
+    }
+
+    public T Peek()
+    {
+        if (_elements.Count == 0)
+        {
+            throw new Exception("Stack is empty.");
+        }
+        return _elements[_elements.Count - 1];
+    }
+}
