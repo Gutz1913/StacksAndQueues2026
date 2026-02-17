b124d746cb5dbd01f8ec92b7826c517e4dc53faa
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-17 11:25:48 -0500

    UI Console for queues using a list

--- StacksAndQueues/QueueUsingList.UI.Console/Program.cs
+++ StacksAndQueues/QueueUsingList.UI.Console/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Queues.Backend;
+using System.ComponentModel.Design;
+
+var queue = new QueueUsingList<int>();
+var opt = string.Empty;
+do
+{
+    opt = Menu();
+    switch (opt)
+    {
+        case "1":
+            try
+            {
+                Console.Write("Enter a number: ");
+                var number = int.Parse(Console.ReadLine()!);
+                queue.Enqueue(number);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            break;
+
+        case "2":
+            try
+            {
+                var item = queue.Dequeue();
+                Console.WriteLine($"Item dequeued: {item}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            break;
+
+        case "3":
+            try
+            {
+                var item = queue.Peek();
+                Console.WriteLine($"Item in front: {item}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            break;
+    }
+}
+while (opt != "0");
+
+string Menu()
+{
+    Console.WriteLine("1. Enqueue");
+    Console.WriteLine("2. Dequeue");
+    Console.WriteLine("3. Peek");
+    Console.WriteLine("0. Exit");
+    Console.Write("Choose an option: ");
+    return Console.ReadLine()!;
+}
